99398d7f8d851083329a9d52d61dcdd371aac2b0
Marc Wickenden <marc@4armed.com>
2021-03-03 07:03:55 +0000

    Update copyright

--- pkg/eks/eks.go
+++ pkg/eks/eks.go
@@ -1,4 +1,4 @@
-// Copyright © 2021 Amiran Alavidze @airman604
+// Copyright © 2021 Amiran Alavidze @airman604 and Marc Wickenden @marcwickenden
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
